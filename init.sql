162fc91e8fd08c53ad1bf96b6f07f1ce7c3d9632
Gaston-Gonzalez <ggonzalez@effectussoftware.com>
2023-05-24 17:52:13 -0300

    init sql upload

---
+++ init.sql
@@ -0,0 +1,76 @@
+CREATE TABLE fifa23 (
+    id BIGINT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    nationality VARCHAR(50),
+    overall INT,
+    club VARCHAR(50),
+    value BIGINT,
+    position VARCHAR(20)
+);
+
+CREATE TABLE fifa22 (
+    id BIGINT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    nationality VARCHAR(50),
+    overall INT,
+    club VARCHAR(50),
+    value BIGINT,
+    position VARCHAR(20)
+);
+
+CREATE TABLE fifa21 (
+    id BIGINT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    nationality VARCHAR(50),
+    overall INT,
+    club VARCHAR(50),
+    value BIGINT,
+    position VARCHAR(20)
+);
+
+CREATE TABLE fifa20 (
+    id BIGINT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    nationality VARCHAR(50),
+    overall INT,
+    club VARCHAR(50),
+    value BIGINT,
+    position VARCHAR(20)
+);
+
+CREATE TABLE fifa19 (
+    id BIGINT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    nationality VARCHAR(50),
+    overall INT,
+    club VARCHAR(50),
+    value BIGINT,
+    position VARCHAR(20)
+);
+
+CREATE TABLE fifa18 (
+    id BIGINT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    nationality VARCHAR(50),
+    overall INT,
+    club VARCHAR(50),
+    value BIGINT,
+    position VARCHAR(20)
+);
+
+CREATE TABLE fifa17 (
+    id BIGINT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    nationality VARCHAR(50),
+    overall INT,
+    club VARCHAR(50),
+    value BIGINT,
+    position VARCHAR(20)
+);
